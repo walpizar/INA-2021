e1a68bab1cb2341f4ec078359e958022ca4d411f
Walter Alpízar <walpizar@gmail.com>
2021-07-29 12:38:14 -0600

    CAMBIO DATOS 1

---
+++ src/Leche.java
@@ -0,0 +1,18 @@
+public class Leche {
+    public static void main(String[] args) {
+       /* 2.	Un productor de leche lleva el 
+        registro de lo que produce en litros, pero cuando entrega 
+        le pagan en galones. Realice un algoritmo, y represéntelo 
+        mediante un diagrama de flujo y el pseudocódigo, que ayude al 
+        productor a saber cuánto recibirá por la entrega de su producción d
+        e un día (1 galón = 3.785 litros).*/
+
+
+        
+
+
+
+
+    }
+    
+}
