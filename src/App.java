9c7d100ad9817b0624c7dd179b716f603ef8d16f
Walter Alpízar <walpizar@gmail.com>
2021-07-29 09:35:04 -0600

    CAMBIO DATOS

--- src/App.java
+++ src/App.java
@@ -1,20 +1,33 @@
 public class App {
     
+    /**
+     * ESTE ESE EL MAIN PRINCIPAL
+     * RECIBE UN PARAMETO DE ARGUMENTOS
+     * RETORN   X COSA
+     * AUTHOR: WALTER ALPIZAR
+     * FEHCA CREACION: 29-7-21
+     * FECHA ULTIMA MODIFICAICON: 29-7-21
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
 
-        //esto es la impresion de el main principal
+        //esto es la impresion de el main principal- linea
         //System.out.println("Hello, World!");
-        /*
+    
+
         caminar();
         if (5>6){
             System.out.println("si");
             int x=5;
         }
-        */
+        
+
+       
     }
 
     public static void caminar(){
-        System.out.println("estoy caminando");
+        System.out.println("estoy caminando, Y CAMBIANDO....");
     }
 
 }
