f0f52df3692e861e8fa6b603efab1615dd5f47a1
Walter Alpízar <walpizar@gmail.com>
2021-07-29 09:19:03 -0600

    inicial

---
+++ src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+
+        //esto es la impresion de el main principal
+        //System.out.println("Hello, World!");
+        /*
+        caminar();
+        if (5>6){
+            System.out.println("si");
+            int x=5;
+        }
+        */
+    }
+
+    public static void caminar(){
+        System.out.println("estoy caminando");
+    }
+
+}
