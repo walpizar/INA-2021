e1434811be7026d46d1cc3b40d72773a52da4f5a
Walter Alpízar <walpizar@gmail.com>
2021-08-05 09:00:44 -0600

    vectores matrices

---
+++ src/clase8/arrays.java
@@ -0,0 +1,51 @@
+package clase8;
+
+import java.util.Scanner;
+
+public class arrays {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //colecciones: vectores y matrices...... array list
+        //declaracion
+        int edad[];
+        short cantidad=0, fil=0, col=0;
+
+        //vector- declaracion e instanciacion 
+       /* int edades[]= new int[10];
+        System.out.println("Digite el tamaño del vector");
+        cantidad= scan.nextShort();
+
+        //instanciacion dinamica deacuerdo al valor indicado por el usuario
+        edad= new int[cantidad];
+*/
+        //matrices
+        //matriz- declaracion e instanciacion 
+        String mNombres[][]= new String[5][3];
+        
+        //declaracion
+        String mApodos[][];
+
+        //tamaño dinamico
+        System.out.println("Digite la cantidad de filas");
+        fil= scan.nextShort();
+
+        System.out.println("Digite la cantidad de columnas");
+        col= scan.nextShort();
+
+        //instanciacion dinamica deacuerdo al valor indicado por el usuario
+        mApodos= new String[fil][col];
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
